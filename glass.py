813492393b89f60f41d5aede3a82b4cd1cb3200b
Your Name <you@example.com>
2024-04-29 10:35:31 -0500

    Working on glass.py

--- glass.py
+++ glass.py
@@ -1,4 +1,4 @@
-#from typing import Tuple
+# from typing import Tuple
 from glass_utilities import *
 from neural import *
 from sklearn.model_selection import train_test_split
@@ -6,4 +6,10 @@ from sklearn.model_selection import train_test_split
 with open ("glass.data", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 7]
 
-print(training_data)
+# print(training_data)
+trd = normalize(training_data)
+# print(trd)
+
+train, test = train_test_split(trd)
+
+nn = NeuralNet
