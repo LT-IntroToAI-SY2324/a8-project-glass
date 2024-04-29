82feee2c2036c99c8a64be64aae6eae96dc4f769
Your Name <you@example.com>
2024-04-29 10:23:27 -0500

    Working on glass.py

--- glass.py
+++ glass.py
@@ -3,3 +3,7 @@ from glass_utilities import *
 from neural import *
 from sklearn.model_selection import train_test_split
 
+with open ("glass.data", "r") as f:
+    training_data = [parse_line(line) for line in f.readlines() if len(line) > 7]
+
+print(training_data)
