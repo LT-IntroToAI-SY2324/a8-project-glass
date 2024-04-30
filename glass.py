fa7dc59a6e59d350a03403b5d502a7e8c8f6d15d
Your Name <you@example.com>
2024-04-30 10:36:40 -0500

    Working on training data

--- glass.py
+++ glass.py
@@ -1,15 +1,15 @@
 # from typing import Tuple
 from glass_utilities import *
-from neural import *
+# from neural import *
 from sklearn.model_selection import train_test_split
 
 with open ("glass.data", "r") as f:
-    training_data = [parse_line(line) for line in f.readlines() if len(line) > 7]
+    training_data = [parse_line(line) for line in f.readlines() if len(line) > 8]
 
-# print(training_data)
-trd = normalize(training_data)
+print(training_data)
+# trd = normalize(training_data)
 # print(trd)
 
-train, test = train_test_split(trd)
+# train, test = train_test_split(trd)
 
-nn = NeuralNet
+# nn = NeuralNet
