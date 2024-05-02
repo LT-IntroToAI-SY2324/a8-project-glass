a97ac92bcbef6a81379fae88a341f6c65a01f4d4
Your Name <you@example.com>
2024-05-02 10:35:36 -0500

    Polishing glass.py

--- glass.py
+++ glass.py
@@ -1,6 +1,6 @@
 # from typing import Tuple
 from glass_utilities import *
-# from neural import *
+from neural import *
 from sklearn.model_selection import train_test_split
 
 with open ("glass.data", "r") as f:
@@ -8,13 +8,17 @@ with open ("glass.data", "r") as f:
 
 # print(training_data)
 trd = normalize(training_data)
-print(trd)
+# print(trd)
 
 train, test = train_test_split(trd)
 
-nn = NeuralNet(11, 7, 1)
-nn.train(train, itters = 10000, print_interval = 1000, learning_rate = 0.2)
+nn = NeuralNet(10, 6, 1)
+nn.train(train, iters = 10000, print_interval = 1000, learning_rate = 0.2)
+
+print(" ")
+print("Types of glass: 0 = Building Windows(Float Processed), 0.2 = Building Windows(Non-Float Processed), 0.4 = Vehicle Windows(Float Processed), 0.6 = Containers, 0.8 = Tableware, 1 = Headlamps")
+print(" ")
 
 for i in nn.test_with_expected(test):
-    diff = round(abs(i[1][0] - i[10][0]), 6)
-    print(f"Desired: {i[1]}, Actual: {i[10]}, Diff: {diff}")
+    diff = round(abs(i[1][0] - i[2][0]), 6)
+    print(f"Desired: {i[1]}, Actual: {i[2]}, Diff: {diff}")
